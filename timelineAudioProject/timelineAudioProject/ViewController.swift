fa64cdcc8c07cafc4571722905edacc65393eb70
brys <brys1saclaus2@gmail.com>
2020-10-29 23:37:17 -0700

    created audio project

---
+++ timelineAudioProject/timelineAudioProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timelineAudioProject
+//
+//  Created by BrysonSaclausa on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello audio world")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
